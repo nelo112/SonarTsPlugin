16261e5c8f79e038c6d785fc29c299eb7e1b1c16
Manuel Huber <manuel.huber@qaware.de>
2015-09-18 13:48:00 +0200

    Invoke Tslint without new shell, which used to fail on unix.

--- src/main/java/com/pablissimo/sonar/TsLintExecutorImpl.java
+++ src/main/java/com/pablissimo/sonar/TsLintExecutorImpl.java
@@ -6,41 +6,43 @@ import org.sonar.api.utils.command.Command;
 import org.sonar.api.utils.command.CommandExecutor;
 import org.sonar.api.utils.command.StreamConsumer;
 
-public class TsLintExecutorImpl implements TsLintExecutor {	
+public class TsLintExecutorImpl implements TsLintExecutor {
 	private static final Logger LOG = LoggerFactory.getLogger(TsLintExecutorImpl.class);
-	
+
 	private StringBuilder stdOut;
 	private StringBuilder stdErr;
-	
+
 	public String execute(String pathToTsLint, String configFile, String file) {
 		LOG.info("TsLint executing for " + file);
 		Command command = Command.create("node");
-		
-		command
-			.addArgument("\"" + pathToTsLint + "\" --config \"" + configFile + "\" --format json \"" + file.trim() + "\"");
-			
-		command.setNewShell(true);
-		
+		command.addArgument(pathToTsLint);
+		command.addArgument("--format");
+		command.addArgument("json");
+		command.addArgument("--config");
+		command.addArgument(configFile);
+		command.addArgument(file.trim());
+		command.setNewShell(false);
+
 		this.stdOut = new StringBuilder();
 		this.stdErr = new StringBuilder();
-		
-		StreamConsumer stdOutConsumer = new StreamConsumer() {			
+
+		StreamConsumer stdOutConsumer = new StreamConsumer() {
 			public void consumeLine(String line) {
 				LOG.trace("TsLint Out: " + line);
 				stdOut.append(line + "\n");
 			}
 		};
-		
+
 		StreamConsumer stdErrConsumer = new StreamConsumer() {
 			public void consumeLine(String line) {
 				LOG.error("TsLint Err: " + line);
 				stdErr.append(line + "\n");
 			}
 		};
-		
+
 		CommandExecutor executor = CommandExecutor.create();
 		int exitCode = executor.execute(command, stdOutConsumer, stdErrConsumer, 5000);
-		
+
 		return stdOut.toString();
 	}
 }
