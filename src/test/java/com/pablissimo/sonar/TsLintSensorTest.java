f04a550445608887bcf2d8bcae46e8d78c5c654b
Alexander Krauss <alexander.krauss@qaware.de>
2015-09-18 14:01:45 +0200

    Ensure that sensor does not fail badly when it is not configured.
    Instead, it simply logs a warning that there is no path to tslint. This avoids failures
    when only some projects in a Sonar installation intend to use the plugin.

--- src/test/java/com/pablissimo/sonar/TsLintSensorTest.java
+++ src/test/java/com/pablissimo/sonar/TsLintSensorTest.java
@@ -42,18 +42,20 @@ public class TsLintSensorTest {
 	FilePredicate predicate;
 	Issuable issuable;
 	IssueBuilder issueBuilder;
-	
+
 	List<File> files;
 	File file;
 	org.sonar.api.resources.File sonarFile;
-	
+
 	TsLintExecutor executor;
 	TsLintParser parser;
 	TsLintSensor sensor;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		this.settings = mock(Settings.class);
+		when(this.settings.getString(TypeScriptPlugin.SETTING_TS_LINT_PATH)).thenReturn("/path/to/tslint");
+
 		this.fileSystem = mock(FileSystem.class);
 		this.perspectives = mock(ResourcePerspectives.class);
 		this.issuable = mock(Issuable.class);
@@ -61,24 +63,24 @@ public class TsLintSensorTest {
 		when(this.issuable.newIssueBuilder()).thenReturn(this.issueBuilder);
 		doReturn(this.issuable).when(this.perspectives).as(eq(Issuable.class), any(org.sonar.api.resources.File.class));
 		this.rulesProfile = mock(RulesProfile.class);
-		
+
 		this.file = mock(File.class);
 		doReturn(true).when(this.file).isFile();
-		
+
 		this.files = new ArrayList<File>(Arrays.asList(new File[] {
 			this.file
 		}));
-		
+
 		this.fileSystem = mock(FileSystem.class);
 		this.predicate = mock(FilePredicate.class);
-		when(fileSystem.files(this.predicate)).thenReturn(this.files);	
-		
+		when(fileSystem.files(this.predicate)).thenReturn(this.files);
+
 		this.filePredicates = mock(FilePredicates.class);
 		when(this.fileSystem.predicates()).thenReturn(this.filePredicates);
 		when(filePredicates.hasLanguage(TypeScriptLanguage.LANGUAGE_EXTENSION)).thenReturn(this.predicate);
-		
+
 		this.sonarFile = mock(org.sonar.api.resources.File.class);
-		
+
 		this.executor = mock(TsLintExecutor.class);
 		this.parser = mock(TsLintParser.class);
 		this.sensor = spy(new TsLintSensor(settings, fileSystem, perspectives, rulesProfile));
@@ -91,29 +93,29 @@ public class TsLintSensorTest {
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	public void shouldExecuteOnProject_ReturnsTrue_WhenAnyTsFiles() {
 		assertTrue(this.sensor.shouldExecuteOnProject(null));
 	}
-	
+
 	@Test
 	public void shouldExecuteOnProject_ReturnsFalse_WhenNoTsFiles() {
 		when(fileSystem.files(this.predicate)).thenReturn(new ArrayList<File>());
 		assertFalse(this.sensor.shouldExecuteOnProject(null));
 	}
-	
+
 	@Test
 	public void analyse_WritesConfigurationFile() throws IOException {
 		when(this.fileSystem.files(any(FilePredicate.class))).thenReturn(new ArrayList<File>());
 		this.sensor.analyse(mock(Project.class), mock(SensorContext.class));
-		
+
 		ArgumentCaptor<File> fileCaptor = ArgumentCaptor.forClass(File.class);
 		verify(this.sensor).writeConfiguration(eq("{\n  \"rules\": {}\n}"), fileCaptor.capture(), eq(Charsets.UTF_8));
-		
+
 		assertEquals("tslint.json", fileCaptor.getValue().getName());
 	}
-	
+
 	@Test
 	public void analyse_addsIssues() {
 		TsLintIssue issue = new TsLintIssue();
@@ -122,9 +124,9 @@ public class TsLintSensorTest {
 
 		TsLintPosition startPosition = new TsLintPosition();
 		startPosition.setLine(1);
-		
+
 		issue.setStartPosition(startPosition);
-		
+
 		TsLintIssue[] issues = new TsLintIssue[] {
 			issue
 		};
@@ -137,11 +139,22 @@ public class TsLintSensorTest {
 				return null;
 			}
 		};
-		
+
 		when(this.issuable.addIssue(any(Issue.class))).then(captureIssue);
 		when(parser.parse(any(String.class))).thenReturn(issues);
 		this.sensor.analyse(mock(Project.class), mock(SensorContext.class));
-		
+
 		assertEquals(1, capturedIssues.size());
+	}
+
+	@Test
+	public void analyse_doesNothingWhenNotConfigured() throws IOException {
+		when(this.settings.getString(TypeScriptPlugin.SETTING_TS_LINT_PATH)).thenReturn(null);
+
+		when(this.fileSystem.files(any(FilePredicate.class))).thenReturn(new ArrayList<File>());
+		this.sensor.analyse(mock(Project.class), mock(SensorContext.class));
+
+		verify(this.sensor, never()).writeConfiguration(anyString(), any(File.class), any(Charset.class));
+		verify(this.issuable, never()).addIssue(any(Issue.class));
 	}
 }
